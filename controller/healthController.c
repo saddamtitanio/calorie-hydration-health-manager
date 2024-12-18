cf88d9c5787ef656501876fd6fd0f1e54f4cc5ea
fahrezaarsyam <fahreza.arsya.m@gmail.com>
2024-12-18 11:31:10 +0700

    created a function to check if user has set a goal or not

--- controller/healthController.c
+++ controller/healthController.c
@@ -28,3 +28,30 @@ void writeHealthFile(int userId, float targetWeight, int targetDays) {
     fprintf(file, buffer);
     closeDBFile(&file, &filePath);
 }
+
+/* Check whether the user has previously set a goal or not */
+bool isNullHealthProfile(int id) {
+    char *filePath = getDbFilePath(HEALTH_FILE);
+    char buffer[2];
+
+    if (filePath == NULL) {
+        perror("Failed to get path.");
+        return false;
+    }
+
+    FILE *file = fopen(filePath, "r");
+
+    if (file == NULL) {
+        perror("Error opening file");
+        free(filePath);
+        return false; 
+    }
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        char *token = strtok(buffer, "|");
+        if (atoi(token) == id) {
+            return true;
+        }
+        token = strtok(NULL, "|");
+    }
+    return false;
+}
